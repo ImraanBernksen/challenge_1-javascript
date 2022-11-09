cb44d4026fd88d1a93a49337870153ce93a9223e
ImraanBernksen <imraanbernksen@gmail.com>
2022-11-09 09:38:07 +0200

    awweee

--- js/code.js
+++ js/code.js
@@ -1,13 +1,43 @@
-let userFirstname = prompt("Please enter your name");
-let userSurname = prompt("Please enter your surname");
-let userSalary = prompt("Please enter your salary");
-let userSalarySum = eval(userSalary) + 100000;
-// let userSalarySum = eval(`${userSalary}`) + 100000;
+// let userFirstname = prompt("Please enter your name");
+// let userSurname = prompt("Please enter your surname");
+// let userSalary = prompt("Please enter your salary");
+// let userSalarySum = eval(userSalary) + 100000;
+// // let userSalarySum = eval(`${userSalary}`) + 100000;
 
-console.log(`Name:${userFirstname}`);
-console.log(`Name:${userSurname}`);
-console.log(`Name:${userSalarySum}`);
+// console.log(`Name:${userFirstname}`);
+// console.log(`Name:${userSurname}`);
+// console.log(`Name:${userSalarySum}`);
 
-document.write(`Name:${userFirstname}`);
-document.write(`<br>Name:${userSurname}`);
-document.write(`<br>Name:${userSalarySum}`);
+// document.write(`Name:${userFirstname}`);
+// document.write(`<br>Name:${userSurname}`);
+// document.write(`<br>Name:${userSalarySum}`);
+
+// First approach: make use of function
+// let firstName = prompt("Please enter your name: ");
+// let surName = prompt("Please enter your surname: ");
+// let salary = prompt("Please enter your salary: ", 0);
+// document.write(`
+// Name: ${firstName}<br>
+// Surname: ${surName}<br>
+// Salary: R${eval(`${salary}*2`)}`);
+
+//My Attempt
+// let userName = prompt("Please enter your name: ");
+// let userSurname = prompt("Please enter your surname: ");
+// let userSalary = prompt("Please enter your salary: ");
+// document.write(`
+// Name: ${userName}<br>
+// Surname: ${userSurname}<br>
+// Salary: R${eval(`${userSalary}/4`)}`);
+
+// function addition() {
+//     return 9 + 2
+// }
+
+// document.write(`Sum: ${addition()}`);
+
+let addition = ()=>{
+    document.write(8 + 4);
+}
+
+addition();
